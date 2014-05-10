284eefed08f0eb8aa77c9ccb7be231d2618ac93f
Matija Stepanic <stepanic.matija@gmail.com>
2014-05-10 15:54:25 +0200

    Login added

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -16,15 +16,46 @@
  */
 
 module.exports = {
-    
-  
-
 
   /**
    * Overrides for the settings in `config/controllers.js`
    * (specific to UserController)
    */
-  _config: {}
+  _config: {},
 
-  
+  /**
+    * Login
+    */
+  login: function (req, res) {
+    var bcrypt = require('bcrypt');
+
+    var status = false;
+
+    User.findOneByEmail(req.body.email).done(function (err, user) {
+      if (err) res.json({ "status": status, error: 'DB error' }, 500);
+
+      if (user) {
+        bcrypt.compare(req.body.password, user.password, function (err, match) {
+          if (err) res.json({ "status": status, error: 'Server error' }, 500);
+
+          if (match) {
+            // password match
+            req.session.user = user.id;
+
+            delete user.password;
+
+            status = true;
+            res.json({ "status": status, data: user });
+          } else {
+            // invalid password
+            if (req.session.user) req.session.user = null;
+            res.json({ "status": status, error: 'Invalid password' }, 400);
+          }
+        });
+      } else {
+        res.json({ "status": status, error: 'User not found' }, 404);
+      }
+    });
+  }
+
 };
